08fcc600905e2dc2351e99a4e975dbad7c550569
Marx.W <boyuan@vt.edu>
2019-10-05 21:02:02 -0400

    main & observer & pointer view

---
+++ main.php
@@ -0,0 +1,27 @@
+
+<?php
+    // session_write_close();
+    session_start();
+
+
+    $_SESSION['P_state'] = 1;
+    $_SESSION['O_state'] = 1;
+    $_SESSION['trial_state'] = 1;
+
+    // // session_write_close();
+    // delay(1);
+    // sleep(3);
+    $i = 0;
+    while($i < 2)
+    {
+        $_SESSION['P_state'] = $_SESSION['P_state'] + 1;
+        $_SESSION['O_state'] = $_SESSION['O_state'] + 1;
+        $_SESSION['trial_state'] = $_SESSION['trial_state'] + 1;
+        sleep(2); 
+        $i = $i + 1;
+    }
+
+
+    session_unset(); 
+    session_destroy(); 
+?>
---
+++ observer_view.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+    session_start();
+?>
+<head>
+    <title>starter</title>
+    <meta charset = "utf-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    <meta name = "viewport" content = "width = device-width, initial-scale = 1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type = "text/css" href="css/observer_view.css">
+</head>
+<body>
+    <div class = "header" id = "state_info">
+        <h1>Observer View</h1>
+        <h2><?php 
+        //echo "Step ".$_SESSION['O_state'];
+        ?></h2>
+
+        
+    </div>
+    <div class = "content" id = "instruction">
+        <p>Please wait for instructions</p>
+        
+        <div class = "form-group">
+                <!-- <input id='btn' name="submit" type='submit' value='Submit'><button type = "submit" class = "btn btn-primary btn-block btn-lg" name = "sign_up"> submit </button> -->
+                <input id='btn' name="submit" type='submit' value='Submit'>
+                
+                    
+                <?php
+                    
+                    if(isset($_POST['submit'])){
+                        header("Location:https://www.formget.com/app/");
+                        print "Hello world!";
+                    }
+                ?>
+        </div>
+    </div>
+
+
+    <?php 
+    
+    //include("main.php")
+    ?>
+
+</body>
+</html>
